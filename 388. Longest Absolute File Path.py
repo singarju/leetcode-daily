b712837a4b065c49ba7fa0de7480b360abb7fb67
arju.singh <singarju@iu.edu>
2025-06-06 22:48:26 -0400

    844. Backspace String Compare

---
+++ 388. Longest Absolute File Path.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        lines=input.split("\n")
+        print(lines)
+        maxlen=0
+        pathlen={0:0}
+        for line in lines:
+            path=line.split("\t")[-1]
+            print(path)
+            depth=len(line)-len(path)
+            if '.' in path:
+                maxlen=max(maxlen, pathlen[depth]+len(path))
+            else:
+                pathlen[depth+1]=pathlen[depth]+1+len(path)
+        return maxlen
+
+
+        
